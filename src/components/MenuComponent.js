6ab9f786f83a0356c0f1e831cb713a1895764837
Mridul <mgupta_bemba18@thapar.edu>
2020-11-07 09:47:50 -0500

    components part1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
 
-class Menu extends Component{
-    constructor(props){
+class Menu extends Component {
+    constructor(props) {
         super(props);
-
         this.state = {
             dishes: [
                 {
@@ -36,39 +35,39 @@ class Menu extends Component{
                   name:'ElaiCheese Cake',
                   image: 'assets/images/elaicheesecake.png',
                   category: 'dessert',
-                  label:'', 
+                  label:'',
+                  price:'2.99',
                   description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
                ],
         };
-
     }
-    
-    render{ 
 
-        //map helps visit every elememnt in the array sequencially
+    render() {
         const menu = this.state.dishes.map((dish) => {
-            return(
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name}/> 
-                        </Media>
-                    </Media> 
-                    <Media body className="ml-5">
-                        <Media heading>{dish.name}</Media>
-                        <p>{dish.description} </p>
-                    </Media>
-                </div>
-            ) ;
+            return (
+              <div key={dish.id} className="col-12 mt-5">
+                <Media tag="li">
+                  <Media left middle>
+                      <Media object src={dish.image} alt={dish.name} />
+                  </Media>
+                  <Media body className="ml-5">
+                    <Media heading>{dish.name}</Media>
+                    <p>{dish.description}</p>
+                  </Media>
+                </Media>
+              </div>
+            );
         });
-        return( 
-            <div className="container">
-                  <div className="row">
-                     <Media  list>
-                        {menu} //variable
-                     </Media>
+
+        return (
+          <div className="container">
+            <div className="row">
+              <Media list>
+                  {menu}
+              </Media>
             </div>
-        ); 
+          </div>
+        );
     }
 }
 
