3265670527ff2ca4d4fb45441af0ea6aecd08da3
Mridul <mgupta_bemba18@thapar.edu>
2020-11-23 07:26:50 -0500

    Fetch errors

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -2,21 +2,19 @@ import * as ActionTypes from './ActionTypes';
 
 export const Comments = (state = {
     errMess: null,
-    comments : []
+    comments: []
 }, action) => {
-    switch(action.type){
-
+    switch (action.type) {
         case ActionTypes.ADD_COMMENTS:
-            return {...state, isLoading:false, errMess: null, comments:action.payload};
+            return { ...state, isLoading: false, errMess: null, comments: action.payload };
 
         case ActionTypes.COMMENTS_FAILED:
-            return {...state, isLoading:false, errMess: action.payload, comments:[]};
+            return { ...state, isLoading: false, errMess: action.payload, comments: [] };
 
         case ActionTypes.ADD_COMMENT:
-                var comment = action.payload;
-                comment.id = state.comments.length;
-                comment.date = new Date().toISOString();
-                return {...state, comments: state.concat(comment)};
+            var comment = action.payload;
+            return { ...state, comments: state.comments.concat(comment) };
+
         default:
             return state;
     }
